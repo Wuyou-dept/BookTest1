c89e239a8b8138c50707d2211d252930c3d2513c
Wuyou-dept <15724570404@163.com>
2025-10-07 15:24:29 +0800

    添加主应用程序类

---
+++ src/main/java/org/example/book1810/Book1810Application.java
@@ -0,0 +1,13 @@
+package org.example.book1810;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book1810Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Book1810Application.class, args);
+    }
+
+}
